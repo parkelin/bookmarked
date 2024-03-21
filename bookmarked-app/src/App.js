be5ba130d0cc1251c7a6f49a371415c568609192
Elin Park <elinpark@utexas.edu>
2024-03-21 17:40:53 -0500

    Added folders and pages for screens

--- bookmarked-app/src/App.js
+++ bookmarked-app/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Welcome from "./pages/Welcome/Welcome.js";
+import LoadingScreen from "./pages/LoadingScreen/LoadingScreen.js";
+import Character from "./pages/Character/Character.js";
+import Glossary from "./pages/Glossary/Glossary.js";
+import WritingDoc from "./pages/WritingDoc/WritingDoc.js";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="global-container">
+      <div className="content-container">
+        <Switch>
+          {/* The Switch decides which component to show based on the current URL.*/}
+          <Route exact path="/">
+            <Welcome />
+          </Route>
+
+          <Route exact path="/loading">
+            <LoadingScreen />
+          </Route>
+
+          <Route exact path="/glossary">
+            <Glossary />
+          </Route>
+
+          <Route exact path="/glossary/character">
+            <Character />
+          </Route>
+
+          <Route exact path="/writingdoc">
+            <WritingDoc />
+          </Route>
+
+        </Switch>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ bookmarked-app/src/pages/Welcome/Welcome.js
@@ -0,0 +1,17 @@
+// Welcome Author
+
+import React from 'react';
+import './Welcome.css'; // Make sure you have this CSS file in your project
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome, Author!</h1>
+        <p>This is your new home screen.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
