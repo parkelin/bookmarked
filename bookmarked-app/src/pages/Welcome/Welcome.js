f9dc661ec9ca8bec15f57982c5e1d7c775fb6544
Elin Park <elinpark@utexas.edu>
2024-03-21 17:53:39 -0500

    linking not working quite yet

--- bookmarked-app/src/pages/Welcome/Welcome.js
+++ bookmarked-app/src/pages/Welcome/Welcome.js
@@ -1,17 +1,11 @@
 // Welcome Author
 
 import React from 'react';
-import './Welcome.css'; // Make sure you have this CSS file in your project
+import '../../App.css';
+import './Welcome.css'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome, Author!</h1>
-        <p>This is your new home screen.</p>
-      </header>
-    </div>
-  );
+export default function Welcome() {
+    return (
+        <><h1>Welcome, Author!</h1><p>This is your new home screen.</p></>
+    );
 }
-
-export default App;
