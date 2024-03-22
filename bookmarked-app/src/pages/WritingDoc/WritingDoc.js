f7f2343dc04f4dcafab4e97ccb208876d405ce5f
Anagha Kenikar <akenikar@utexas.edu>
2024-03-22 10:54:23 -0500

    added the navbar

--- bookmarked-app/src/pages/WritingDoc/WritingDoc.js
+++ bookmarked-app/src/pages/WritingDoc/WritingDoc.js
@@ -1,1 +1,17 @@
 // Writing Doc + Sidebar
+
+import React from 'react';
+import '../../App.css';
+import Navbar from '../../components/Navbar';
+
+export default function WritingDoc() {
+    return (
+        <div className="welcome">
+            <Navbar />
+            <div className="main-content">
+                <><h1>Writing Doc</h1></>
+            </div>
+        </div>
+       
+    );
+}
