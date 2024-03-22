d47da9861e925a35fca37516ef087144bab6af62
reyvachawla <reyvachawla@Reyvas-MacBook-Pro.local>
2024-03-22 11:00:25 -0500

    text editor added

---
+++ bookmarked-app/src/pages/WritingDoc/TextEditor.js
@@ -0,0 +1,30 @@
+import { useCallback } from 'react'
+import Quill from 'quill'
+import 'quill/dist/quill.snow.css';
+import './WritingDoc.css'
+
+const TOOLBAR_OPTIONS = [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ font: [] }],
+    [{ list: "ordered" }, { list: "bullet" }],
+    ["bold", "italic", "underline"],
+    [{ color: [] }, { background: [] }],
+    [{ script: "sub" }, { script: "super" }],
+    [{ align: [] }],
+    ["image", "blockquote", "code-block"],
+    ["clean"], 
+]
+
+
+export default function TextEditor() {
+
+    const wrapperRef = useCallback(wrapper => {
+        if (wrapper == null) return
+        wrapper.innerHTML = ''
+        const editor = document.createElement("div")
+        wrapper.append(editor)
+        new Quill (editor, {theme: "snow", modules: { toolbar: TOOLBAR_OPTIONS} })
+    }, [])
+    return <div className="container" ref = {wrapperRef}></div>
+    
+}
--- bookmarked-app/src/pages/WritingDoc/WritingDoc.js
+++ bookmarked-app/src/pages/WritingDoc/WritingDoc.js
@@ -1,1 +1,11 @@
 // Writing Doc + Sidebar
+
+import React from 'react';
+import '../../App.css';
+import TextEditor from './TextEditor'
+
+function WritingDoc() {
+    return <TextEditor />
+}
+
+export default WritingDoc
