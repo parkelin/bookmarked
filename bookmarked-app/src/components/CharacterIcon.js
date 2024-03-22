471c6902c5e11f803f212989bd877b137ab125ad
Huyen <hcnguyen02@gmail.com>
2024-03-22 12:30:14 -0500

    implemented unique character pages

--- bookmarked-app/src/components/CharacterIcon.js
+++ bookmarked-app/src/components/CharacterIcon.js
@@ -2,16 +2,21 @@ import React from 'react'
 import '../pages/Glossary/Glossary.css'
 import RoundedRectangle from './RoundedRectangle'
 import ThreeDotsIcon from './ThreeDotsIcon'
+import { Link } from 'react-router-dom';
 
 const CharacterIcon = ({ name, iconImage, onClick }) => {
+    const characterPagePath = `/glossary/${encodeURIComponent(name)}`
     return (
-        <div className="character-icon-container">
-            <RoundedRectangle>
-                <ThreeDotsIcon />
-                <img src={require(`../images/${iconImage}`)} className="character-image" alt={`${name} icon`}/>
-            </RoundedRectangle>
-            <h3 className="character-name">{name}</h3>
-        </div>
+        <Link to={characterPagePath} className="character-icon-container">
+            <div className="character-icon-container">
+                <RoundedRectangle onClick={onClick}>
+                    <ThreeDotsIcon />
+                    <img src={require(`../images/${iconImage}`)} className="character-image" alt={`${name} icon`}/>
+                </RoundedRectangle>
+                <h3 className="character-name">{name}</h3>
+            </div>
+        </Link>
+        
     )
 }
 export default CharacterIcon
