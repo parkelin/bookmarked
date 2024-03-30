807dd6734e31ce9897a945b4bc05606422433c11
Huyen <hcnguyen02@gmail.com>
2024-03-30 12:35:38 -0500

    hide secrets

--- bookmarked-app/src/firebase-config.js
+++ bookmarked-app/src/firebase-config.js
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth'; // aunthentification
 // import 'firebase/firestore'; // database
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBtyux7r4OVb0h2dpg3OT6rE8qmz28Xkok",
-    authDomain: "bookmarked-c4d8f.firebaseapp.com",
-    projectId: "bookmarked-c4d8f",
-    storageBucket: "bookmarked-c4d8f.appspot.com",
-    messagingSenderId: "425137796052",
-    appId: "1:425137796052:web:11c6e270a0244a65f15c41",
-    measurementId: "G-MSN954WENR"
+  const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
   };
   
   // Initialize Firebase
